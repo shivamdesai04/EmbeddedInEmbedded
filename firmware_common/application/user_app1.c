a2bafe3eb31e2dc9cc0b4bb10deeaa4989bd1038
shivamdesai04 <101665374+shivamdesai04@users.noreply.github.com>
2023-12-28 01:18:35 -0700

    init

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -92,6 +92,9 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  PWMAudioSetFrequency(BUZZER1, 500);
+  
+  
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -140,7 +143,12 @@ State Machine Function Definitions
 /* What does this state do? */
 static void UserApp1SM_Idle(void)
 {
-    
+  if(IsButtonPressed(BUTTON0)) {
+    PWMAudioOn(BUZZER1);
+  }
+  else {
+    PWMAudioOff(BUZZER1);
+  }
 } /* end UserApp1SM_Idle() */
      
 
