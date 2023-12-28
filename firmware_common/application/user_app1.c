4b2f3c8ef0e878d896af9b6f49e0fd5df918d74e
shivamdesai04 <101665374+shivamdesai04@users.noreply.github.com>
2023-12-28 03:29:25 -0700

    Revert "Revert "Revert "bug"""
    This reverts commit da4cd194449ab81b25ea75b5e445b96b5461c4af.

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -92,13 +92,8 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
-  LedOff(GREEN);
-  LedOff(PURPLE);
-  LedOff(BLUE);
-  LedOff(CYAN);
-  LedOff(ORANGE);
-  LedOff(YELLOW);
- 
+  
+  
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -148,20 +143,39 @@ State Machine Function Definitions
 static void UserApp1SM_Idle(void)
 {
   if(WasButtonPressed(BUTTON0)) {
-      ButtonAcknowledge(BUTTON0);
-      PWMAudioSetFrequency(BUZZER1, 262);
-    }
+    ButtonAcknowledge(BUTTON0);
+    PWMAudioSetFrequency(BUZZER1, 262);
+  }
   
   
+  if(WasButtonPressed(BUTTON1)) {
+    ButtonAcknowledge(BUTTON1);
+    PWMAudioSetFrequency(BUZZER1, 294);
+  }
   
- if(IsButtonPressed(BUTTON0) || IsButtonPressed(BUTTON1) || IsButtonPressed(BUTTON2) || IsButtonPressed(BUTTON3)) {
+ 
+  if(WasButtonPressed(BUTTON2)) {
+    ButtonAcknowledge(BUTTON2);
+    PWMAudioSetFrequency(BUZZER1, 330);
+  }
+  
+  
+  if(WasButtonPressed(BUTTON3)) {
+    ButtonAcknowledge(BUTTON3);
+    PWMAudioSetFrequency(BUZZER1, 392);
+  }
+  
+  
+  if(IsButtonPressed(BUTTON0) || IsButtonPressed(BUTTON1) || IsButtonPressed(BUTTON2) || IsButtonPressed(BUTTON3)) {
     
     PWMAudioOn(BUZZER1);
   }
   else {
     PWMAudioOff(BUZZER1);
   }
- 
+  
+  
+  
   
 } /* end UserApp1SM_Idle() */
      
